a6df626396f32786d2af3146f0490d86fd64c22f
Dennis Wegereef <dennis.wegereef@deptagency.com>
2023-05-01 17:55:28 +0200

    Init Sanity Studio and Next JS

---
+++ apps/studio/martijn-portfolio/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'j8doku4y',
+    dataset: 'production'
+  }
+})
---
+++ apps/studio/martijn-portfolio/sanity.config.ts
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+//import {googleMapsInput} from '@sanity/google-maps-input'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Martijn Portfolio',
+
+  projectId: 'j8doku4y',
+  dataset: 'production',
+
+  plugins: [
+    deskTool(),
+    visionTool(),
+    //googleMapsInput(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
